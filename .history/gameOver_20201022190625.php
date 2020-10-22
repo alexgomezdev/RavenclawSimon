afe76ade8cbf24bddbcf62250164ac01eac6a992
silviadlc <57746795+silviadlc@users.noreply.github.com>
2020-10-22 19:36:35 +0200

    [ADD] exit & gameOVer frames

---
+++ .history/gameOver_20201022190625.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Ravenclaw Simon</title>
+    <link rel="stylesheet" href="templates/gameOver.css">
+</head>
+<body>
+    <header>
+        HOLA
+    </header>
+    <main>
+        <div id="title">
+            <h1>Ravenclaw Simon</h1>
+        </div>
+        <div class="container">
+            <div id="img">
+                
+            </div>
+            <div id="scroll">
+                <span id="game-over" >
+                    GAME OVER
+                </span>
+             </div>
+        </div>
+        <div id="button_play">
+            <button><h3>Inicio</h3></button>
+            <button><h3>Siguiente Nivel</h3></button>
+        </div>
+        <div id="name_introduction">
+
+        </div>
+    </main>
+    <footer>
+        ADIOS
+    </footer>
+	<script type="text/javascript" src="main.js"></script>
+</body>
+</html>
---
+++ .history/index_20201022185458.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Ravenclaw Simon</title>
+    <link rel="stylesheet" href="templates/index.css">
+</head>
+<body bgcolor="white" onLoad="textticker()">
+    <header>
+        HOLA
+    </header>
+    <main>
+        <div id="title">
+            <h1>Ravenclaw Simon</h1>
+        </div>
+        <div class="container">
+            <div id="img">
+                
+            </div>
+            <div id="scroll1">
+                <span id="congrats" >
+                    NIVEL SUPERADO
+                </span>
+             </div>
+        </div>
+        <div id="button_play">
+            <button><h3>Inicio</h3></button>
+            <button><h3>Siguiente Nivel</h3></button>
+        </div>
+        <div id="name_introduction">
+
+        </div>
+    </main>
+    <footer>
+        ADIOS
+    </footer>
+	<script type="text/javascript" src="main.js"></script>
+</body>
+</html>
