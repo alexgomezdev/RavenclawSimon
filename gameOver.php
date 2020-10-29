4428f8b604931e4709b905692f8d7f7ad3fbda42
byrambo16 <byrambo166@gmail.com>
2020-10-29 19:09:16 +0100

    (ADD) levels code into succes&gameover files

--- gameOver.php
+++ gameOver.php
@@ -3,6 +3,7 @@
 session_start();
 ?>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,7 @@ session_start();
     <link rel="stylesheet" href="templates/gameOver.css">
     <link rel="stylesheet" href="templates/general.css">
 </head>
+
 <body>
     <header>
         <div id="home_link_div">
@@ -19,18 +21,25 @@ session_start();
     <main>
         <div id="show_name">
             <?php
-                if (isset($_SESSION['user'])) {
-                    echo 'Jugador: '.$_SESSION['user'];
-                }
+            $archivo = file('templates/config.txt');
+            $nivel = explode(";", $archivo[$_SESSION['lvl']]);
+            if (isset($_SESSION['user'])) {
+                echo 'Jugador: ' . $_SESSION['user'];
+            }
             ?>
         </div>
         <div class="container">
             <div id="scroll">
                 <span>G</span><span>A</span><span>M</span><span>E</span><span>&nbsp;</span><span>O</span><span>V</span><span>E</span><span>R</span>
             </div>
+            <div id="code">
+                <?php echo '<p> Código: ' . $nivel[4] . '</p>' ?>
+            </div>
         </div>
         <div id="button_div">
-            <button id="button_play" onclick="location.href='start.php'" accesskey="v"><h3>VOLVER A PROVAR</h3></button>
+            <button id="button_play" onclick="location.href='start.php'" accesskey="v">
+                <h3>VOLVER A PROVAR</h3>
+            </button>
         </div>
     </main>
     <footer>
@@ -38,6 +47,7 @@ session_start();
             &#0169 2020 - Creado por: Silvia de la Cruz, Álex Gomez e Iker Cayero
         </div>
     </footer>
-	<script type="text/javascript" src="main.js"></script>
+    <script type="text/javascript" src="main.js"></script>
 </body>
+
 </html>
--- success.php
+++ success.php
@@ -3,6 +3,7 @@
 session_start();
 ?>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,7 @@ session_start();
     <link rel="stylesheet" href="templates/success.css">
     <link rel="stylesheet" href="templates/general.css">
 </head>
+
 <body>
     <header>
         <div id="home_link_div">
@@ -19,19 +21,29 @@ session_start();
     <main>
         <div id="show_name">
             <?php
-                if (isset($_SESSION['user'])) {
-                    echo 'Jugador: '.$_SESSION['user'];
-                }
+            $archivo = file('templates/config.txt');
+            $nivel = explode(";", $archivo[$_SESSION['lvl']]);
+            if (isset($_SESSION['user'])) {
+                echo 'Jugador: ' . $_SESSION['user'];
+            }
             ?>
         </div>
         <div class="container">
             <div id="scroll">
-                <h1><span>¡</span><span>N</span><span>I</span><span>V</span><span>E</span><span>L</span><span> </span><span>S</span><span>U</span><span>P</span><span>E</span><span>R</span><span>A</span><span>D</span><span>O</span><span>!</span></h1>            
+                <h1><span>¡</span><span>N</span><span>I</span><span>V</span><span>E</span><span>L</span><span> </span><span>S</span><span>U</span><span>P</span><span>E</span><span>R</span><span>A</span><span>D</span><span>O</span><span>!</span></h1>
+            </div>
+            <div id="code">
+                <?php echo '<p> Código: ' . $nivel[4] . '</p>' ?>
             </div>
         </div>
+
         <div id="button_div">
-            <button id="button_play" onclick="location.href='start.php'" accesskey="v"><h3>VOLVER A PROVAR</h3></button>
-            <button id="button_play" onclick="location.href='start.php'" accesskey="n"><h3>SIGUIENTE NIVEL</h3></button>
+            <button id="button_play" onclick="location.href='start.php'" accesskey="v">
+                <h3>VOLVER A PROVAR</h3>
+            </button>
+            <button id="button_play" onclick="location.href='start.php?do=next'" accesskey="n">
+                <h3>SIGUIENTE NIVEL</h3>
+            </button>
         </div>
     </main>
     <footer>
@@ -39,6 +51,7 @@ session_start();
             &#0169 2020 - Creado por: Silvia de la Cruz, Álex Gomez e Iker Cayero
         </div>
     </footer>
-	<script type="text/javascript" src="main.js"></script>
+    <script type="text/javascript" src="main.js"></script>
 </body>
+
 </html>
